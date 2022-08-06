830df64b8d7008a35d3a1f66f0c68fc0d91e4c14
ganesh tawde <95049416+GANESH2202@users.noreply.github.com>
2022-08-06 14:45:40 +0000

    Initial commit

---
+++ Problem1.js
@@ -0,0 +1,9 @@
+function valuecal(result){
+    calculatorForm.evalresult.value = calculatorForm.evalresult.value + result;
+}
+
+function clr() {
+    calculatorForm.evalresult.value = " " ;
+   }
+ 
+ 
